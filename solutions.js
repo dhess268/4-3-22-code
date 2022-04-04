c0c7a21d30b7c007f9c6f6851e3ffab9e87c591f
Dillon Hess <dhess268@gmail.com>
2022-04-03 22:17:37 -0400

    add more solutions

--- solutions.js
+++ solutions.js
@@ -9,3 +9,18 @@ function billboard(name, price = 30){
     }
     return total;
   } 
+
+
+
+// Converts an array of ascii codes to the string representations
+
+  var ArrowFunc = function(arr) {
+    return arr.map(item => String.fromCharCode(item)).join(''); //Complete this function
+  }
+
+
+
+// Squared each element in an array and then returns the sum of all the results
+ function squareSum(numbers){
+   return (numbers.length > 0) ? numbers.reduce((acc, current) => acc += Math.pow(current, 2), 0): 0;
+}
